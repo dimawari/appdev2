13c5f868125b32441515c3302bb5499ac447b841
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2026-04-17 16:39:04 +0800

    Done with going back

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button } from "@react-navigation/elements";
 import {
   createStaticNavigation,
   useNavigation,
-} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button } from '@react-navigation/elements';
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text, View } from "react-native";
 
 function HomeScreen() {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
 
-      <Button onPress={() => navigation.navigate('Details')}>
+      <Button onPress={() => navigation.navigate("Details")}>
         Go to Details
       </Button>
     </View>
@@ -25,18 +24,20 @@ function DetailsScreen() {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
 
-      <Button onPress={() => navigation.push('Details')}>
+      <Button onPress={() => navigation.push("Details")}>
         Go to Details... again
       </Button>
+
+      <Button onPress={() => navigation.goBack()}>Go back</Button>
     </View>
   );
 }
 
 const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Home',
+  initialRouteName: "Home",
   screens: {
     Home: HomeScreen,
     Details: DetailsScreen,
