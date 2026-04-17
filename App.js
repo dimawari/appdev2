afc99c45c32dfefed2b3ffcf1713435186d53cf0
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2026-04-17 16:37:27 +0800

    Done with navigating to a screen multiple times

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import { Button } from "@react-navigation/elements";
+import * as React from 'react';
+import { View, Text } from 'react-native';
 import {
   createStaticNavigation,
   useNavigation,
-} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Text, View } from "react-native";
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from '@react-navigation/elements';
 
 function HomeScreen() {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
 
-      <Button onPress={() => navigation.navigate("Details")}>
+      <Button onPress={() => navigation.navigate('Details')}>
         Go to Details
       </Button>
     </View>
@@ -21,15 +22,21 @@ function HomeScreen() {
 }
 
 function DetailsScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+
+      <Button onPress={() => navigation.push('Details')}>
+        Go to Details... again
+      </Button>
     </View>
   );
 }
 
 const RootStack = createNativeStackNavigator({
-  initialRouteName: "Home",
+  initialRouteName: 'Home',
   screens: {
     Home: HomeScreen,
     Details: DetailsScreen,
