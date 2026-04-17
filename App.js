0703eae7c4b347909e7872ea36f2d230c38c16c4
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2026-04-17 16:42:07 +0800

    Done with initial params

--- App.js
+++ App.js
@@ -57,7 +57,10 @@ function DetailsScreen() {
 const RootStack = createNativeStackNavigator({
   screens: {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Details: {
+      screen: DetailsScreen,
+      initialParams: { itemId: 42 },
+    },
   },
 });
 
