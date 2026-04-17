9c1eb1817a0d872cc4bc8c68740f80be3000d489
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2026-04-17 16:20:19 +0800

    Configuring the navigator

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { createStaticNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Text, View } from "react-native";
-
-const Stack = createNativeStackNavigator();
 
 function HomeScreen() {
   return (
@@ -20,13 +18,16 @@ function DetailsScreen() {
   );
 }
 
+const RootStack = createNativeStackNavigator({
+  initialRouteName: "Home",
+  screens: {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
